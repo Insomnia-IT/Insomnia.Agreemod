d1b394e6d9bec6a6ae62cf45b5295aace5b84504
root <a@cherepanov.info>
2022-06-26 14:10:38 +0300

    fix run

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ NAME=agreemod
 
 docker build --no-cache -t $NAME .
 docker rm -f $NAME
-docker run -d -p 5500:80 --restart=always --name $NAME $NAME
+docker run -d  --network=apps -p 5500:80 --restart=always --name $NAME $NAME
 
